a9af4aaf5e8a42153bc1d332491028e15d5d40aa
budsabajanjaroen <47513804+budsabajanjaroen@users.noreply.github.com>
2024-10-15 18:11:56 +0700

    Add files via upload
    edit css html

---
+++ about.php
@@ -0,0 +1,27 @@
+<section class="" id="about">
+  <div class="container">
+    <div class="row align-items-center py-5">
+      <div class="col-md-8">
+        <h1>เกี่ยวกับเรา</h1>
+        <p>
+        โปรตีนพืชมีไขมันดี คอเลสเตอรอลต่ำ ใยอาหารสูง อิ่มท้อง ไม่มีแลคโตส 
+         ผู้บริโภคที่มีความเสี่ยงเกี่ยวกับโรคหัวใจและหลอดเลือด โรคเบาหวาน โรคมะเร็ง
+          ผู้ที่ใส่ใจต่อสุขภาพ และผู้ที่แพ้แลคโตสในนม จึงหันมานิยมบริโภคมากขึ้นในการเสริมโปรตีนให้ร่างกาย
+          มาตรฐานที่อุ่นใจสำหรับอาหารฮาลาล โปรตีนพืชสำหรับผู้สูงวัยและผู้ป่วย
+        </p>
+
+        <a href="#carousel_93f2"
+          class="u-active-custom-color-3 u-border-2 u-border-active-white u-border-hover-white  u-btn u-btn-round u-button-style u-custom-color-7 u-hover-custom-color-2 u-radius-50 u-text-active-white u-text-hover-white u-btn-1">ร่วมงานกับเรา
+        </a>
+        &nbsp;&nbsp;
+        <a href="https://liff.line.me/2004905932-ZvVLn72n"
+          class="u-active-custom-color-3 u-border-2 u-border-active-white u-border-hover-white  u-btn u-btn-round u-button-style u-custom-color-7 u-hover-custom-color-2 u-radius-50 u-text-active-white u-text-hover-white u-btn-2">สนใจซื้อสินค้า
+        </a>
+        <br> <br>
+      </div>
+      <div class="col-md-4">
+        <img src="images/12.10.svg" alt="">
+      </div>
+    </div>
+  </div>
+</section>
---
+++ ingredient.php
@@ -0,0 +1,334 @@
+   <!-- ======= Services Section ======= -->
+   <div id="services" class="services-area area-padding">
+     <div class="container">
+       <div class="row">
+         <div class="col-md-12 col-sm-12 col-xs-12">
+           <div class="section-headline services-head text-center">
+             <h2>ส่วนประกอบหลักที่สำคัญ</h2>
+           </div>
+         </div>
+       </div>
+
+       <div class="row text-center">
+         <!-- Start Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/1.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>โปรตีนจากถั่วลันเตา</h4>
+                 <p>
+                   โปรตีนสกัดจากถั่วลันเตาเป็นโปรตีนจากพืชที่เป็นแหล่งสารอาหารที่มีกรดอะมิโนที่สำคัญและจำเป็นคือ BCAAs ซึ่งมีส่วนสำคัญในการสร้างกล้ามเนื้อและมีส่วนช่วยในการย่อยและการดูดซึมสารอาหารเพื่อนำไปใช้เป็นพลังงาน </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/2.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>โปรตีนถั่วเหลือง</h4>
+                 <p>
+                   โปรตีนถั่วเหลืองประกอบด้วยกรดอะมิโนที่จำเป็นหลายชนิดและมีไฟโตเอสโตรเจนในปริมาณที่สูงจึงช่วยบำรุงผิวพรรณและมวลกระดูกอีกทั้งยังมีสารไฟโตสเทอเรสและซาโพนินในปริมาณมากซึ่งมีส่วนช่วยในการลดระดับคอเลสเตอรอลและยังมีส่วนช่วยป้องกันโรคหลอดเลือดและหัวใจ </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/3.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ถั่วอัลมอนด์</h4>
+                 <p>
+                   มีปริมาณคาร์โบไฮเดรตต่ำแต่มีอาหารสูงซึ่งมีส่วนช่วยควบคุมปริมาณน้ำตาลในเส้นเลือดจึงช่วยลดความเสี่ยงของการเกิดโรคอ้วนลงพุงโรคเบาหวานชนิดที่ 2 โรคหลอดเลือดหัวใจและโรคหัวใจโรคมะเร็งทั้งยังมีปริมาณไขมันดีชนิด hdl สูงจึงมีส่วนช่วยควบคุมระดับคอเลสเตอรอลในเลือด </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/4.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ถั่ววอลนัท</h4>
+                 <p>
+                   เป็นแหล่งของไขมันโปรตีนและแร่ธาตุหลายชนิดทั้งวิตามินบี 6 วิตามินเคไฟเบอร์ซิงค์เซเรเนียมและไทเทเนียมและอื่นๆทั้งยังอุดมไปด้วยโอเมก้า 3 ซึ่งเป็นกรดไขมันจำเป็นที่มีประโยชน์ต่อสมองและหัวใจ </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/5.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ถั่วบราซิล</h4>
+                 <p>
+                   มีแร่ธาตุที่สำคัญคือแร่เซเรเนียมซึ่งมีปริมาณที่สูงกว่าถั่วชนิดอื่นถึง 8 เท่ามีคุณสมบัติเป็นสารต้านอนุมูลอิสระที่มีส่วนช่วยต้านการก่อตัวของมะเร็งได้อย่างมีประสิทธิภาพสูงอีกทั้งยังมีส่วนช่วยสร้างระบบภูมิคุ้มกันให้แข็งแรง </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/6.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ดาวอินคา</h4>
+                 <p>
+                   มีกรดไขมันที่จำเป็นคือโอเมก้า 3 และโอเมก้า 6 มีส่วนช่วยในการบำรุงสมองและอุดมไปด้วยโทรโคฟีร่อนที่มีคุณสมบัติเป็นสารต้านอนุมูลอิสระประสิทธิภาพสูงอีกทั้งยังมีไฟโตสเตอเรสซึ่งมีส่วนช่วยในการลดคอเลสเตอรอลชนิดเลว ldl ทั้งในเลือดและทางเดินอาหาร </p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+       </div>
+       <div class="row text-center">
+         <!-- Start Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/cho/7.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ผงโกโก้ (Theobroma Cacao)</h4>
+                 <p>
+                   เนื่องจากพอลิฟีนอลมีหน้าที่ในการต้านอนุมูลอิสระและเบต้าเซลล์จะช่วยบรรเทาอาการของภาวะซึมเศร้าและช่วยให้ลดไขมันในเลือดได้</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/1.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>โปรตีนไข่ขาว Egg Albumin</h4>
+                 <p>
+                   โภชนาการโปรตีนชนิดดูดึมได้ช้าแต่ต่อเนื่อง ที่เหมาะสำหรับ ผู้มีความเปราะบางในตับ, ไต และ NCDs</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/3.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ข้าวสาลี Wheat Powder </h4>
+                 <p>
+                   มีส่วนช่วยลดความดันโลหิตสูง ช่วยขยายหลอดเลือด ช่วยลดระดับน้ำตาลในเลือด, ลดระดับคอเลสเตอรอลในเลือด, ลดไขมันในเลือด</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/4.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ข้าวโอ๊ต Oat Powder</h4>
+                 <p>
+                   ช่วยแก้อาการท้องผูก หรือผู้ที่มีปัญหา เกี่ยวกับระบบขับถ่ายได้เป็นอย่างดี เพราะมีการขับถ่ายที่สม่ำเสมอขึ้นอีกด้วย</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/5.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>อิริทริทอล Erythritol </h4>
+                 <p>
+                   สารให้ความหวานที่ไม่กระทบต่อระดับอินซูลินในเลือด แคลอรี่เท่ากับ 0 โดยน้ำตาลอิริทริทอล จะถูกดูดซึมอย่างรวดเร็วในลำไส้เล็ก</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/6.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>สารสกัดจากกัวกัม Partially Hydrolyses Guar Gum</h4>
+                 <p>
+                   มีสารต้านอนุมูลอิสระ มีส่วนช่วยเรื่องอาการเบื่ออาหาร</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+       </div>
+
+
+       <div class="row text-center">
+         <!-- Start Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/13.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>สารสกัดอัลฟาฟ่า Alfalfa Extract </h4>
+                 <p>
+                   เนื่องจากพอลิฟีนอลมีหน้าที่ในการต้านอนุมูลอิสระและเบต้าเซลล์จะช่วยบรรเทาอาการของภาวะซึมเศร้าและช่วยให้ลดไขมันในเลือดได้</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/7.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>บัควีท Buckwheat Powder</h4>
+                 <p>
+                   ต้านการอักเสบ ต้านไวรัส ต้านจุลินทรีย์ และต้านโรคเนื้อร้าย รักษาโรคภูมิแพ้ช่วยให้อาการของการแพ้ละอองต่างๆ</p>
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/2.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>มิลเล็ต Common Millet Powder</h4>
+                 <p>
+                   ช่วยให้ระบบการย่อยอาหาร เช่น ลำไส้ กระเพาะอาหารทำงานได้ดี ช่วยขับปัสสาวะ ช่วยรักษาอาการของโรคอหิวาตกโรค
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/9.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ข้าวกล้อง Brown Rice Powder</h4>
+                 <p>
+                   วิตามินบีรวม ช่วยป้องกันและบรรเทาอาการอ่อนเพลีย แขน ขาไม่มีแรง ปวดกล้ามเนื้อ โรคผิวหนังบางชนิด บำรุงสมองทำให้เจริญอาหาร
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/10.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ข้าวบาร์เลย์ Barley Powder</h4>
+                 <p>
+                   อุดมไปด้วยแคลเซียมในปริมาณสูง จึงช่วยเสริมสร้างกระดูกป้องกันโรคกระดูกพรุน อีกทั้งยังช่วยสร้างคอลลาเจนที่ทำให้กระดูกและข้อต่อมีความยืดหยุ่น
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+         <!-- End Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <!-- end col-md-4 -->
+           <div class=" about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/11.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>ผงน้ำมันรำข้าว Refined Rice Bran Oil Powder </h4>
+                 <p>
+                   บำรุงระบบประสาทเเละสมอง ในน้ำมันรำข้าว มีกรดไลโนเลอิก
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+       </div>
+
+       <div class="row text-center">
+         <!-- Start Left services -->
+         <div class="col-md-4 col-sm-4 col-xs-12">
+           <div class="about-move">
+             <div class="services-details">
+               <div class="single-services">
+                 <a class="services-icon" href="">
+                   <img src="images/egg/12.png" style="width: 50%; height: 50%; " alt="">
+                 </a>
+                 <h4>วิตามินรวม</h4>
+                 <p>
+                   ช่วยบำรุงระบบประสาท บำรุงสมอง ช่วยเสริมสร้างการเจริญเติบโต บำรุงกล้ามเนื้อ ช่วยให้หัวใจทำงานเป็นปกติ
+               </div>
+             </div>
+             <!-- end about-details -->
+           </div>
+         </div>
+
+
+       </div>
+
+     </div>
+   </div>
